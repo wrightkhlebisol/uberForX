5ed17ab6cd2286ce58e1a74a2450401cf4e00c9d
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-04 11:30:50 +0100

    socket request for help

--- socket-events.js
+++ socket-events.js
@@ -6,14 +6,36 @@ function initialize(server) {
 	const io = require("socket.io")(server);
 
 	io.on("connection", (socket) => { // Listen on the 'connection' event for incoming sockets
-		console.log("A user just connected");
+		console.log("A user just connected", socket.id, socket.rooms);
 
 		socket.on("join", (data) => { // Listen to any join event from connected users
 			socket.join(data.userId); // User joins a unique room/channel that's named after the userId
 			console.log(`${data.userType} joined room: ${data.userId}`);
 		});
-	})
 
+		socket.on("request-for-help", async (requestDetails) => {
+			// Save the request
+			const requestTime = new Date(); // Time of request
+			const requestId = new mongoose.Types.ObjectId(); // generate unique ID for the request
+
+			const location = { // convert lati and long to [long, lat]
+				coordinates: [
+					requestDetails.location.longitude,
+					requestDetails.location.latitude
+				],
+				address: requestDetails.location.address
+			}
+
+			await dbOps.saveRequest(requestId, requestTime, location, requestDetails.civilianId, "waiting");
+
+			const nearestCops = await dbOps.fetchNearestCops(location.coordinates, 2000);
+			requestDetails.requestId = requestId;
+			for (let i = 0; i < nearestCops.length; i++){
+				console.log("Emiting request help");
+				io.sockets.to(nearestCops[i].userId).emit('request-for-help', requestDetails);
+			}
+		});
+	});
 }
 
 module.exports = { initialize };
